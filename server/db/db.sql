00a118ebaa4990e16a04040e870cf40a3e84ee11
Katherine <chiakyu@gmail.com>
2021-12-27 17:48:44 -0800

    Set up Express Server and Link to PostGres

---
+++ server/db/db.sql
@@ -0,0 +1,103 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL NOT NULL PRIMARY KEY,
+  auth_id VARCHAR(50) NOT NULL UNIQUE,
+  username VARCHAR(30) NOT NULL UNIQUE,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(100) NOT NULL,
+  cardholder_name	VARCHAR(100),
+  card_number VARCHAR(64),
+  card_exp_date DATE,
+  billing_address1 VARCHAR(100),
+  billing_address2 VARCHAR(100),
+  billing_city VARCHAR(100),
+  billing_state VARCHAR(10),
+  billing_postal_code VARCHAR(15),
+  billing_country VARCHAR(40)
+);
+
+DROP TABLE IF EXISTS projects CASCADE;
+CREATE TABLE projects (
+  id SERIAL NOT NULL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  tagline VARCHAR(200) NOT NULL,
+  description VARCHAR(2000),
+  funding REAL NOT NULL,
+  deadline DATE NOT NULL,
+  auth_id VARCHAR(50) NOT NULL REFERENCES users(auth_id)
+);
+
+DROP TABLE IF EXISTS users_projects;
+CREATE TABLE users_projects (
+  id SERIAL NOT NULL PRIMARY KEY,
+  user_funding REAL NOT NULL,
+  project_id SERIAL NOT NULL REFERENCES projects(id),
+  auth_id VARCHAR(50) NOT NULL REFERENCES users(auth_id)
+);
+
+
+INSERT INTO users (
+  username,
+  auth_id,
+  email,
+  cardholder_name,
+  card_number,
+  card_exp_date,
+  billing_address1,
+  billing_address2,
+  billing_city,
+  billing_state,
+  billing_postal_code,
+  billing_country )
+  VALUES (
+    'aTest123',
+    '1234567',
+    'adam@email.com',
+    'Adam Apple',
+    '1234567980',
+    '2024-03-01',
+    '123 Main St',
+    '123',
+    'Fullerton',
+    'CA',
+    '92832',
+    'USA');
+
+
+UPDATE users
+    SET (
+      first_name,
+      last_name,
+      email,
+      cardholder_name,
+      card_number,
+      card_exp_date,
+      billing_address1,
+      billing_address2,
+      billing_city,
+      billing_state,
+      billing_postal_code,
+      billing_country
+    ) = (
+      'Adam',
+      'Apple',
+      'adam@email.com',
+      'Adam Apple',
+      '1234567980',
+      '2024-03-01',
+      '123 Main St',
+      '123',
+      'Fullerton',
+      'CA',
+      '92832',
+      'USA')
+  WHERE id = 1;
+
+SELECT * FROM projects
+  LEFT JOIN (
+    SELECT project_id,
+    TRUNC(SUM(funding)) as total_funding
+    FROM users_projects
+    GROUP BY project_id
+  ) users_projects on users_projects.project_id = projects.id;
---
+++ server/server.js
@@ -0,0 +1,143 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const db = require("./db");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+// Get all projects
+app.get("/api/v1/projects", async (req, res) => {
+  try {
+    //const results = await db.query("select * from projects");
+    const projectFundingData = await db.query(
+      "select * from projects left join (select project_id, COUNT(*), TRUNC(AVG(funding),1) as average_funding from users group by project_id) users on projects.id = users.project_id;"
+    );
+
+    res.status(200).json({
+      status: "success",
+      results: projectFundingData.rows.length,
+      data: {
+        projects: projectFundingData.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Get a project
+app.get("/api/v1/projects/:id", async (req, res) => {
+  console.log(req.params.id);
+
+  try {
+    const project = await db.query(
+      "select * from projects left join (select project_id, COUNT(*), TRUNC(AVG(funding),1) as average_funding from users group by project_id) users on projects.id = users.project_id where id = $1",
+      [req.params.id]
+    );
+    // select * from projects wehre id = req.params.id
+
+    const users = await db.query(
+      "select * from users where project_id = $1",
+      [req.params.id]
+    );
+    console.log(users);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        project: project.rows[0],
+        users: users.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Create a project
+
+app.post("/api/v1/projects", async (req, res) => {
+  console.log(req.body);
+
+  try {
+    const results = await db.query(
+      "INSERT INTO projects (title, tagline, funding) values ($1, $2, $3) returning *",
+      [req.body.title, req.body.tagline, req.body.funding]
+    );
+    console.log(results);
+    res.status(201).json({
+      status: "success",
+      data: {
+        project: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Update projects
+
+app.put("/api/v1/projects/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "UPDATE projects SET title = $1, tagline = $2, funding = $3 where id = $4 returning *",
+      [req.body.title, req.body.tagline, req.body.funding, req.params.id]
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        project: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(req.params.id);
+  console.log(req.body);
+});
+
+// Delete project
+
+app.delete("/api/v1/projects/:id", async (req, res) => {
+  try {
+    const results = db.query("DELETE FROM projects where id = $1", [
+      req.params.id,
+    ]);
+    res.status(204).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// add funding for a project
+app.patch("/api/v1/projects/:id/fund", async (req, res) => {
+  try {
+    const newFunding = await db.query(
+      "INSERT INTO users_projects (project_id, users_id, funding) values ($1, $2, $3) returning *;",
+      [req.params.id, req.body.project_id, req.body.users_id, req.body.funding]
+    );
+    console.log(fund);
+    res.status(201).json({
+      status: "success",
+      data: {
+        project: fund.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}`);
+});
